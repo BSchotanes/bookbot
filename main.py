00b678f95875b367eb1e8d9c208ce996171dc391
BSchotanes <bryan@schotanes.com>
2025-03-08 20:22:39 -0800

    Added Book Anaylsis from args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count, character_count, sort_dictonary
+import sys 
 
 def get_book_text(filepath):
     contents_of_file = []
@@ -7,7 +8,11 @@ def get_book_text(filepath):
     return contents_of_file
 
 def main():
-    path = "books/frankenstein.txt"
+    args = sys.argv
+    if(len(args) < 2):
+        sys.exit(1)
+
+    path = sys.argv[1]
     booktext = get_book_text(path)
     word_count = get_word_count(booktext)
     counted_characters = character_count(booktext)
