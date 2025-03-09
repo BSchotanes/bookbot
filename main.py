3d649af9fbf3cedea2a45eadbe99e6e93acfe8d0
BSchotanes <bryan@schotanes.com>
2025-03-08 20:10:03 -0800

    Print Report and Update gitignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_word_count, character_count
+from stats import get_word_count, character_count, sort_dictonary
+
 def get_book_text(filepath):
     contents_of_file = []
     with open(filepath) as f:
@@ -6,8 +7,25 @@ def get_book_text(filepath):
     return contents_of_file
 
 def main():
-   word_count = get_word_count(get_book_text("books/frankenstein.txt"))
-   print(f"{word_count} words found in the document")
-   print(character_count(get_book_text("books/frankenstein.txt")))
+    path = "books/frankenstein.txt"
+    booktext = get_book_text(path)
+    word_count = get_word_count(booktext)
+    counted_characters = character_count(booktext)
+    chars_sorted = sort_dictonary(counted_characters)
+    print(f"{word_count} words found in the document")
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    
+    # Print only alphabetical characters
+    for char_dict in chars_sorted:
+        char = char_dict["char"]
+        if char.isalpha():  # Only print alphabetical characters
+            count = char_dict["count"]
+            print(f"{char}: {count}")
+    
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,16 @@ def character_count(booktext):
             alphas[text.lower()]  = 1
     return alphas
 
+def sort_dictonary(alpha_dictionary):
+    list_of_dict = []
+
+    for character in alpha_dictionary:
+        char_dict = {"char": character, "count": alpha_dictionary[character]}
+        list_of_dict.append(char_dict)
+
+    list_of_dict.sort(reverse=True, key=sort_on)
+
+    return list_of_dict
+
+def sort_on(alpha_dictionary):
+    return alpha_dictionary["count"]
